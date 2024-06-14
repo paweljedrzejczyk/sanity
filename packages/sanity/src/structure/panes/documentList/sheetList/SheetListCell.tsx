0fb18db78061107a6703dda7d688a0a1b7175fe0
Pedro Bonamin <pedrobonamin@gmail.com>
2024-06-14 08:38:01 -0300

    feat(structure): show validation icon only on cell hover

--- packages/sanity/src/structure/panes/documentList/sheetList/SheetListCell.tsx
+++ packages/sanity/src/structure/panes/documentList/sheetList/SheetListCell.tsx
@@ -27,7 +27,7 @@ const CellValidation = ({validation}: {validation: FormNodeValidation[]}) => {
     return null
   }
   return (
-    <ValidationIconContainer>
+    <ValidationIconContainer id="validation-icon">
       <FormFieldValidationStatus validation={validation} fontSize={0} />
     </ValidationIconContainer>
   )
@@ -75,6 +75,16 @@ const DataCell = styled.td<DataCellProps>((props) => {
       transition: box-shadow 0.1s;
       box-shadow: inset 0px 0px 0px ${$cellState === 'focused' ? 2 : 1}px
         var(--card-focus-ring-color);
+    }
+
+    & #validation-icon {
+      opacity: 0;
+      transition: opacity 0.2s ease;
+    }
+    :hover {
+      & #validation-icon {
+        opacity: 1;
+      }
     }
   `
 })
